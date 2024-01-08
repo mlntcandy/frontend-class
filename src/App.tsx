ca4d847f71dc5a2d7231564852edce13ceeed97b
mlntcandy <me@mlntcandy.com>
2024-01-09 00:12:12 +0300

    Add example columns

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Table } from "antd";
+import { Table, Tag } from "antd";
 import { type ColumnsType } from "antd/es/table";
 
 interface DataType {
@@ -6,6 +6,8 @@ interface DataType {
   name: string;
   age: number;
   address: string;
+  tags: string[];
+  occupation: string;
 }
 
 const tableColumns: ColumnsType<DataType> = [
@@ -24,6 +26,31 @@ const tableColumns: ColumnsType<DataType> = [
     dataIndex: "address",
     key: "address",
   },
+  {
+    title: "Tags",
+    dataIndex: "tags",
+    key: "tags",
+    render: (_, { tags }) => (
+      <>
+        {tags.map((tag) => {
+          let color = tag.length > 5 ? "geekblue" : "green";
+          if (tag === "loser") {
+            color = "volcano";
+          }
+          return (
+            <Tag color={color} key={tag}>
+              {tag.toUpperCase()}
+            </Tag>
+          );
+        })}
+      </>
+    ),
+  },
+  {
+    title: "Occupation",
+    dataIndex: "occupation",
+    key: "occupation",
+  },
 ];
 
 const tableData: DataType[] = [
@@ -32,12 +59,16 @@ const tableData: DataType[] = [
     name: "Mike",
     age: 32,
     address: "10 Downing Street",
+    tags: ["nice", "developer"],
+    occupation: "Software Engineer",
   },
   {
     key: "2",
     name: "John",
     age: 42,
     address: "10 Downing Street",
+    tags: ["great", "artist"],
+    occupation: "Artist",
   },
 ];
 
