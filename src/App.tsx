738d869967cfe20522fc178b773cb65ed8d1736a
mlntcandy <me@mlntcandy.com>
2024-01-08 23:40:30 +0300

    Add Table component to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,44 @@
 import { useState } from "react";
 import "./App.css";
+import { Table } from "antd";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Table
+        dataSource={[
+          {
+            key: "1",
+            name: "Mike",
+            age: 32,
+            address: "10 Downing Street",
+          },
+          {
+            key: "2",
+            name: "John",
+            age: 42,
+            address: "10 Downing Street",
+          },
+        ]}
+        columns={[
+          {
+            title: "Name",
+            dataIndex: "name",
+            key: "name",
+          },
+          {
+            title: "Age",
+            dataIndex: "age",
+            key: "age",
+          },
+          {
+            title: "Address",
+            dataIndex: "address",
+            key: "address",
+          },
+        ]}
+      />
     </>
   );
 }
