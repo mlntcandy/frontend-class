0d99a473259c180d0f995ca76f9cfe3aadc7925b
mlntcandy <me@mlntcandy.com>
2024-01-09 00:03:22 +0300

    Move data out of component

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,42 @@
 import { Table } from "antd";
 
+const tableColumns = [
+  {
+    title: "Name",
+    dataIndex: "name",
+    key: "name",
+  },
+  {
+    title: "Age",
+    dataIndex: "age",
+    key: "age",
+  },
+  {
+    title: "Address",
+    dataIndex: "address",
+    key: "address",
+  },
+];
+
+const tableData = [
+  {
+    key: "1",
+    name: "Mike",
+    age: 32,
+    address: "10 Downing Street",
+  },
+  {
+    key: "2",
+    name: "John",
+    age: 42,
+    address: "10 Downing Street",
+  },
+];
+
 function App() {
   return (
     <>
-      <Table
-        dataSource={[
-          {
-            key: "1",
-            name: "Mike",
-            age: 32,
-            address: "10 Downing Street",
-          },
-          {
-            key: "2",
-            name: "John",
-            age: 42,
-            address: "10 Downing Street",
-          },
-        ]}
-        columns={[
-          {
-            title: "Name",
-            dataIndex: "name",
-            key: "name",
-          },
-          {
-            title: "Age",
-            dataIndex: "age",
-            key: "age",
-          },
-          {
-            title: "Address",
-            dataIndex: "address",
-            key: "address",
-          },
-        ]}
-      />
+      <Table dataSource={tableData} columns={tableColumns} />
     </>
   );
 }
