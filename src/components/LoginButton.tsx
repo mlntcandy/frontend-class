23b7f3eb04e0e6e9c38d50298763dc82199b29d2
mlntcandy <me@mlntcandy.com>
2024-01-09 21:46:37 +0300

    Added basic styles to LoginButton

--- src/components/LoginButton.tsx
+++ src/components/LoginButton.tsx
@@ -1,11 +1,20 @@
 import { useAuth } from "../lib/auth/context";
+import styled from "styled-components";
+
+const StyledButton = styled.button`
+  background-color: #eee;
+  border: 1px solid #ccc;
+  border-radius: 0.25em;
+  padding: 0.5em 1em;
+  cursor: pointer;
+`;
 
 export const LoginButton: React.FC = () => {
   const { loggedIn, login, logout } = useAuth();
 
   return (
-    <button onClick={loggedIn ? logout : login}>
+    <StyledButton onClick={loggedIn ? logout : login}>
       {loggedIn ? "Выйти" : "Войти"}
-    </button>
+    </StyledButton>
   );
 };
