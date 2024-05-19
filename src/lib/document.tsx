9ccfe3cc69e830013992e575fb5780b2c2675215
mlntcandy <me@mlntcandy.com>
2024-05-19 21:20:10 +0300

    added pdf template

---
+++ src/lib/document.tsx
@@ -0,0 +1,41 @@
+import {
+  StyleSheet,
+  Document,
+  Page,
+  View,
+  Text,
+  Image,
+} from "@react-pdf/renderer";
+
+const styles = StyleSheet.create({
+  page: {
+    flexDirection: "row",
+    backgroundColor: "#E4E4E4",
+  },
+  section: {
+    margin: 10,
+    padding: 10,
+    flexGrow: 1,
+  },
+});
+
+interface IPdfDocumentProps {
+  name: string;
+  picture?: FileList;
+}
+
+export const PdfDocument: React.FC<IPdfDocumentProps> = ({ name, picture }) => {
+  return (
+    <Document>
+      <Page size="A4" style={styles.page} wrap>
+        <View style={styles.section}>
+          <Text>{name}</Text>
+        </View>
+
+        <View style={styles.section}>
+          {picture && picture.length > 0 && <Image source={picture[0]} />}
+        </View>
+      </Page>
+    </Document>
+  );
+};
